a7dab082cb1a25d584236b021715ee1ab84f5e03
Sylvia <sylviaprabudy@gmail.com>
2020-08-08 14:15:36 -0500

    add viewAllEmployees, viewAllRoles, viewAllDeparments functions

--- index.js
+++ index.js
@@ -3,3 +3,36 @@ const db = require("./db");
 
 
 init();
+
+// View all employees
+function viewAllEmployees() {
+    db.allEmployees()
+        .then(([rows]) => {
+            let employees = rows;
+            console.log("\n");
+            console.table(employees);
+        })
+        .then(() => loadMainPrompts());
+}
+
+// View all roles
+function viewAllRoles() {
+    db.allRoles()
+        .then(([rows]) => {
+            let roles = rows;
+            console.log("\n");
+            console.table(roles);
+        })
+        .then(() => loadMainPrompts());
+}
+
+// View all deparments
+function viewallDepartments() {
+    db.allDepartments()
+        .then(([rows]) => {
+            let departments = rows;
+            console.log("\n");
+            console.table(departments);
+        })
+        .then(() => loadMainPrompts());
+}
