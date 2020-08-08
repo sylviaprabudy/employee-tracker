ba5a851ae3f8fca7567771152afd1fdc717d931f
Sylvia <sylviaprabudy@gmail.com>
2020-08-08 13:56:16 -0500

    create allManagers, allEmployees, allRoles, addEmployee, deleteEmployee

--- db/index.js
+++ db/index.js
@@ -1,6 +1,49 @@
 const connection = require("./connection");
 
 class employeeDB {
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    // Find all employees
+    allEmployees() {
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+    }
+
+    // Add an employee
+    addEmployee(employee) {
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+    }
+
+    // Delete an employee
+    deleteEmployee(employeeId) {
+        return this.connection.promise().query(
+            "DELETE FROM employee WHERE id = ?",
+            employeeId
+        );
+    }
+
+    // Find all managers
+    allManagers(employeeId) {
+        return this.connection.promise().query(
+            "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+            employeeId
+        );
+    }
+
+    // Find all roles
+    allRoles() {
+        return this.connection.promise().query(
+            "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+        );
+    }
+
+    // Create a new role
+    createRole(role) {
+        return this.connection.promise().query("INSERT INTO role SET ?", role);
+    }
 
 }
 
